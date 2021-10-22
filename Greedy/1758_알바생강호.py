877d5936c534f761a01900a69c0b735eccf2e251
SOO JEONG KIM <sookelly@korea.ac.kr>
2021-10-22 23:07:15 +0900

    Add files via upload

---
+++ Greedy/1758_알바생강호.py
@@ -0,0 +1,4 @@
+Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:37:30) [MSC v.1927 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/USER/AppData/Local/Programs/Python/Python38-32/1758_알바생강호.py
