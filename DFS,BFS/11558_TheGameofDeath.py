5011875eb34e7d3c5efa53ea37fea28b1cca73bd
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-01-26 23:51:13 +0900

    Add files via upload

---
+++ DFS,BFS/11558_TheGameofDeath.py
@@ -0,0 +1,23 @@
+#11558_TheGameofDeath
+
+def dfs(node,N):
+    for i in a[node]:
+        if visited[i]==0:
+            visited[i]=visited[node]+1
+            dfs(i,N)
+            
+T=int(input())
+
+for _ in range(T):
+    
+    N=int(input())
+    a=[[] for _ in range(N+1)]
+    for i in range(1,N+1):
+        a[i].append(int(input()))
+
+    visited=[0]*(N+1)
+    dfs(1,N)
+    if visited[N] > 0:
+        print(visited[N])
+    else:
+        print(0)
