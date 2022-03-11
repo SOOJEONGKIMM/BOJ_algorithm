ac535083f835a06c38620513e54d20536f1cf659
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-03-12 00:01:42 +0900

    Update 1987_ 알파벳.py

--- DFS,BFS/1987_ 알파벳.py
+++ DFS,BFS/1987_ 알파벳.py
@@ -10,17 +10,17 @@ def dfs(x,y,cnt):
     for i in range(4):
         tx = x + dx[i]
         ty = y + dy[i]
-        if 0<=tx<r and 0<=ty<r and visited[ord(a[tx][ty])-65]!=1:
-            visited[ord(a[tx][ty])-65]=1
+        if 0<=tx<r and 0<=ty<r and visited[a[tx][ty]]!=1:
+            visited[a[tx][ty]]=1
             ncnt = cnt+1
             dfs(tx,ty,ncnt)
-            visited[ord(a[tx][ty])-65]=0
+            visited[a[tx][ty]]=0
             
 r,c = map(int, sys.stdin.readline().split())
 a=[]
 for _ in range(r):
-    a.append(sys.stdin.readline())
+    a.append(list(map(lambda x:ord(x)-65, sys.stdin.readline().rstrip())))
 visited = [0]*26
-visited[ord(a[0][0])-65]=1
+visited[a[0][0]]=1
 dfs(0,0,1)
 print(ans)
