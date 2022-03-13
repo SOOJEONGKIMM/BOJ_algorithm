f6999bf8184dc6a8ac2ee4e74d00824f16e7b7c2
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-03-13 22:47:34 +0900

    Add files via upload

--- DFS,BFS/1987_ 알파벳.py
+++ DFS,BFS/1987_ 알파벳.py
@@ -1,7 +1,7 @@
 #11657_타임머신
 import sys
-dx=[1,-1,0,0]
-dy=[0,0,1,-1]
+dx=(-1,1,0,0)
+dy=(0,0,-1,1)
 ans=0
 def dfs(x,y,cnt):
     global ans
@@ -10,17 +10,20 @@ def dfs(x,y,cnt):
     for i in range(4):
         tx = x + dx[i]
         ty = y + dy[i]
-        if 0<=tx<r and 0<=ty<r and visited[a[tx][ty]]!=1:
-            visited[a[tx][ty]]=1
+        if 0<=tx<r and 0<=ty<c and not a[tx][ty] in alphas:
+            alphas.add(a[tx][ty])
             ncnt = cnt+1
             dfs(tx,ty,ncnt)
-            visited[a[tx][ty]]=0
+            alphas.remove(a[tx][ty])
             
 r,c = map(int, sys.stdin.readline().split())
 a=[]
 for _ in range(r):
     a.append(list(map(lambda x:ord(x)-65, sys.stdin.readline().rstrip())))
-visited = [0]*26
-visited[a[0][0]]=1
+#print(a)
+#visited = [0]*26
+alphas=set()
+alphas.add(a[0][0])
+#visited[a[0][0]]=1
 dfs(0,0,1)
 print(ans)
