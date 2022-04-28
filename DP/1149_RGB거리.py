6d171057f0586b5b21debd34e786fee255f5abd8
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-04-28 11:30:52 +0900

    Add files via upload

---
+++ DP/1149_RGB거리.py
@@ -0,0 +1,12 @@
+#1149_RGB거리
+n=int(input())
+rgb=[]
+for _ in range(n):
+    rgb.append(list(map(int,input().split())))
+
+for i in range(1, len(rgb)):
+    rgb[i][0] = min(rgb[i-1][1], rgb[i-1][2])+rgb[i][0]
+    rgb[i][1] = min(rgb[i-1][0], rgb[i-1][2])+rgb[i][1]
+    rgb[i][2] = min(rgb[i-1][0], rgb[i-1][1])+rgb[i][2]
+
+print(min(rgb[n-1]))
