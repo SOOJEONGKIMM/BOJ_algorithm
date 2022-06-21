9975353aee09e435c6ac801c3732c22e918402fb
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-06-21 15:37:29 +0900

    Add files via upload

---
+++ Implementation/1149_rgb거리.py
@@ -0,0 +1,16 @@
+#1149
+n=int(input())
+rgb=[]
+for _ in range(n):
+	rgb.append(list(map(int,input().split())))
+
+for i in range(1,n):
+	rgb[i][0]=min(rgb[i-1][1], rgb[i-1][2])+rgb[i][0]
+	rgb[i][1]=min(rgb[i-1][0], rgb[i-1][2])+rgb[i][1]
+	rgb[i][2]=min(rgb[i-1][1], rgb[i-1][0])+rgb[i][2]
+
+	
+print(min(rgb[n-1]))
+
+	
+
