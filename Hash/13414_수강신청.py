f2fdbb005cc5439cb62992c8dd41781ebce4a45a
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-06-22 16:11:17 +0900

    Update 13414_수강신청.py

--- Hash/13414_수강신청.py
+++ Hash/13414_수강신청.py
@@ -1,14 +1,15 @@
 #13414_수강신청
-k,l = map(int,input().split())
+import sys
+k,l = map(int,sys.stdin.readline().split())
 num=dict()
 for i in range(l):
-    num[input()]=i    
+    num[sys.stdin.readline()]=i    
 
-num2 = sorted(num.items(), key=lambda x: x[1])
+
 
 cnt=0
-for i in num2:
+for i in sorted(num.items(), key=lambda x: x[1]):
     if cnt>=k:
         break
     cnt+=1
-    print(i[0])
+    print(i[0], end='')
