3ec979e5c87184af3010931c0ee7a87df9be062d
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-10-25 00:35:16 +0900

    Update 18353_병사배치하기.py

--- LIS/18353_병사배치하기.py
+++ LIS/18353_병사배치하기.py
@@ -1,5 +1,16 @@
+#18353
+import sys
+n=int(sys.stdin.readline())
+a=list(map(int,sys.stdin.readline().split()))
 
 
 
+dp = [1] * n
+
+for i in range(n):
+    for j in range(i):
+        if a[i] < a[j]: #감소하는 부분수열 
+            dp[i] = max(dp[i], dp[j]+1)
 
 
+print(len(a) -  max(dp))
