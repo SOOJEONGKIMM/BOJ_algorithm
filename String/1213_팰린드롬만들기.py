4f04e1882a997b8094a93ffff07a352c34452087
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-12-28 00:10:37 +0900

    Update 1213_팰린드롬만들기.py

--- String/1213_팰린드롬만들기.py
+++ String/1213_팰린드롬만들기.py
@@ -1,4 +1,3 @@
-
 import sys
 from collections import Counter
 a = sys.stdin.readline().rstrip()
@@ -6,12 +5,13 @@ d = Counter(a)
 s=""
 for k, v in d.items():
     if v%2==1:
-      if len(s)>0:
-          print("I'm Sorry Hansoo")
-          break
-      s=k
-ans=''
-for k,v in sorted(d.items()):
-    ans+=k*(v//2)
-ans+= s + ans[::-1]
-print(ans)
+        if len(s)>0:
+            print("I'm Sorry Hansoo")
+            break
+        s=k
+else:
+    ans=''
+    for k,v in sorted(d.items()):
+        ans+=k*(v//2)
+    ans+= s + ans[::-1]
+    print(ans)
