bae27687eeafacba14f17b90590e0cd1ab97d6d9
SOO JEONG KIM <sookelly@korea.ac.kr>
2022-12-30 00:18:18 +0900

    Update 17413_단어뒤집기2_.py

--- String/17413_단어뒤집기2_.py
+++ String/17413_단어뒤집기2_.py
@@ -1,1 +1,27 @@
+import sys
+s = sys.stdin.readline().rstrip()
+tmp=[]
+ans=""
+flag=0
+for i in s:
+    tmp.append(i)
+    if i==' ' and flag==0:
+        tmp=tmp[:-1]
+        ans+=''.join(tmp[::-1])+' '
+        tmp=[]
+    elif i=='<':
+        flag=1
+        tmp=tmp[:-1]
+        ans+=''.join(tmp[::-1])
+        ans+='<'
+        tmp=[]
+    elif i=='>':
+        flag=0
+        tmp=tmp[:-1]
+        ans+=''.join(tmp)
+        ans+='>'
+        tmp=[]
+	
 
+ans+=''.join(tmp[::-1])
+print(ans)
