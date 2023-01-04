de1db58f28fce0779568d295f7bc4f6ac86588e8
SOO JEONG KIM <sookelly@korea.ac.kr>
2023-01-05 00:09:53 +0900

    Update 9996_한국이그리울땐서버에접속하지.py

--- String/9996_한국이그리울땐서버에접속하지.py
+++ String/9996_한국이그리울땐서버에접속하지.py
@@ -1,1 +1,13 @@
-
+#정규표현식으로 패턴 반환
+import re, sys
+n=int(input())
+f,l=sys.stdin.readline().rstrip().split('*')
+pt = re.compile(f+'.*'+l+'+')
+for _ in range(n):
+    file=sys.stdin.readline().rstrip()
+    a = pt.search(file)
+    if a and a.group() == file:
+	    print("DA")
+    else:
+	    print("NE")
+    
